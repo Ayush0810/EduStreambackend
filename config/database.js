79f25d2489061a5dbc4652ca4eeb188b2c1af57a
espcommando <98390427+espcommando@users.noreply.github.com>
2023-03-30 21:58:07 +0530

    edustream backend changes

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 mongoose. set('strictQuery', false); 
 export const connectDB  =  ()=>
 {
-  const { connection } =  mongoose.connect(process.env.MONGO_URI);
-  console.log("mongoDB connected with port ",connection.host)
+  const connection =  mongoose.connect(process.env.MONGO_URI);
+  console.log("mongoDB connected with port ",process.env.PORT)
 }
